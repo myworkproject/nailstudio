3b4c067cad15e40d50c3485bf5f9f668350ab021
OleksandrDres <oleksandr.dres@gmail.com>
2016-12-09 18:20:13 +0200

    Adds employee entity

---
+++ src/main/java/com/projects/salon/entity/Employee.java
@@ -0,0 +1,11 @@
+package com.projects.salon.entity;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+    private int id;
+    private String name;
+    private String phone;
+    private int salary;
+}
