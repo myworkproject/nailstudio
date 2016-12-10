e935f974eac4a125424e8b0eea09af0fa58622e9
OleksandrDres <oleksandr.dres@gmail.com>
2016-12-10 20:39:03 +0200

    Adds event entity

---
+++ src/main/java/com/projects/salon/entity/Event.java
@@ -0,0 +1,20 @@
+package com.projects.salon.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+    private Integer id;
+    private String title;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime start;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime end;
+}
