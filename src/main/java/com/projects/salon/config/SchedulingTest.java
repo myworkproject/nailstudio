59c768e0a1a730a1e1f40485de8c53fbe0d47f53
Oleksandr Dres <oleksandr.dres@gmail.com>
2017-01-31 22:05:56 +0200

    Change: scheduler time

--- src/main/java/com/projects/salon/config/SchedulingTest.java
+++ src/main/java/com/projects/salon/config/SchedulingTest.java
@@ -32,7 +32,7 @@ public class SchedulingTest {
     private MailSender mailSender;
 
     @Async
-    @Scheduled(cron = "0 0 22 * * *")
+    @Scheduled(cron = "0 15 22 * * *")
     public void test() {
         List<Employee> employees = employeeRepository.getAllWithoutAdmin();
         for (Employee employee : employees) {
