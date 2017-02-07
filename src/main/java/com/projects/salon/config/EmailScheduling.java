d2ecb414487f06f8322910f457fd2d8f47d09a3e
work.sasha.dres@gmail.com <work.sasha.dres@gmail.com>
2017-02-07 14:08:40 +0200

    Change time to send email

--- src/main/java/com/projects/salon/config/EmailScheduling.java
+++ src/main/java/com/projects/salon/config/EmailScheduling.java
@@ -32,7 +32,7 @@ public class EmailScheduling {
     private MailSender mailSender;
 
     @Async
-    @Scheduled(cron = "0 40 22 * * *")
+    @Scheduled(cron = "0 0 22 * * *")
     public void test() {
         List<Employee> employees = employeeRepository.getAllWithoutAdmin();
         for (Employee employee : employees) {
