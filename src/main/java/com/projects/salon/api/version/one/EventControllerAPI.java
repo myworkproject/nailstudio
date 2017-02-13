bec2acd1fb0b27cd12e72656cf57ecac184315a0
work.sasha.dres@gmail.com <work.sasha.dres@gmail.com>
2017-02-13 18:38:42 +0200

    Adds event controllerAPI

--- src/main/java/com/projects/salon/api/version/one/EventControllerAPI.java
+++ src/main/java/com/projects/salon/api/version/one/EventControllerAPI.java
@@ -1,5 +1,26 @@
 package com.projects.salon.api.version.one;
 
+import com.projects.salon.repository.EmployeeRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/v0.1/events")
+@Slf4j
 public class EventControllerAPI {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @PostMapping
+    public ResponseEntity saveEvent(@RequestParam String clientId, @RequestParam String title) {
+        log.info("SAVE EVENT FROM VIBER: client={}, title={}.", clientId, title);
+        return new ResponseEntity(HttpStatus.OK);
+    }
 }
